8a804976f73cc81d8cd2a3da3c297857cfe13f4b
Imperial Design <amsanchez@knox.edu>
2021-01-16 02:48:21 -0600

    add resume Data to use in components

---
+++ src/resumeData.js
@@ -0,0 +1,92 @@
+let resumeData = {
+    "name": "Alannah Sanchez",
+    "role": "IT Project Manager and Fullstack Web-Developer",
+    "email": "amsanchez@knox.edu",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {"name":"github",
+        "url":"https://github.com/asanchez325?tab=repositories",
+        "className":"github"
+        },
+        {"name":"instagram",
+        "url":"@alannah_de_sanchez",
+        "className":"instagram"
+        }
+    ],
+    "aboutme":"I have a diverse background and a thirst for knowledge. As an IT Project Manager, I initially began web-development to improve my technical skills and aid me in the workplace; however, once I started coding, I caught the love-bug- literally. The first bug I received when coding, resulted in hours of research, stress, more research, and an obsession to improve my skills. I enjoy the challenge and believe that if it doesn't challenge you, it doesn't change you. My diverse background has aided me in my developer journey and I look forward to continue learning and growing as a developer.",
+    "education":[
+        {
+          "UniversityName":"Georgetown University",
+          "specialization":"Masters of Professional Studies",
+          "MonthOfPassing":"Expected December",
+          "YearOfPassing":"2021",
+          "Achievements":"Emergency and Disaster Management"
+        },
+        {
+          "UniversityName":"Knox College",
+          "specialization":"Bachelor of Arts",
+          "MonthOfPassing":"June",
+          "YearOfPassing":"2018",
+          "Achievements":"Major in Neuroscience with Minors in Diversity Studies and Psychology"
+        },
+        {
+          "UniversityName":"Certifications",
+          "specialization":"Certified SCRUM Master-2020",
+          "MonthOfPassing":"June",
+          "YearOfPassing":"2018",
+          "Achievements":"FullStack Web Developer-Expected 2021"
+        }
+      ],
+      "work":[
+        {
+          "CompanyName":"Los Alamos National Laboratory",
+          "specialization":"IT Project Management",
+          "MonthOfLeaving":"In Progress",
+          "YearOfLeaving":"2018",
+          "Achievements":"LANL Distinguished Student Award"
+        },
+        {
+          "CompanyName":"Los Alamos National Laboratory",
+          "specialization":"Occupational Medicine",
+          "MonthOfLeaving":"December",
+          "YearOfLeaving":"2020",
+        }
+      ],
+      "skillsDescription":"Your skills here",
+      "skills":[
+        {
+          "skillname":"HTML5"
+        },
+        {
+          "skillname":"CSS"
+        },
+        {
+          "skillname":"Reactjs"
+        }
+      ],
+      "portfolio":[
+        {
+          "name":"project1",
+          "description":"mobileapp",
+          "img": {computerImage}
+        },
+        {
+          "name":"project2",
+          "description":"mobileapp",
+          "img": {computerImage}
+        },
+        {
+          "name":"project3",
+          "description":"mobileapp",  
+          "img": {computerImage}
+        },
+        {
+          "name":"project4",
+          "description":"mobileapp",
+          "img src": {computerImage}
+        }
+      ],
+
+    }
+    
+    export default resumeData
