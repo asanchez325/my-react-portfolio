fd3f7ab3f84dadfe77f04986b39e1aa1f654908d
Imperial Design <amsanchez@knox.edu>
2021-01-16 07:30:28 -0600

    reformat folders to add js styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 import React, { useState } from 'react';
 import Header from './components/Nav';
 import About from './components/About/About';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Resume from './components/Resume/Resume';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() { 
   const [currentTab, setCurrentTab] = useState("about");
