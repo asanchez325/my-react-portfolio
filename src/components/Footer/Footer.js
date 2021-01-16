289e1f23cd85d47530c7d7d4eb9b8d9626519699
Imperial Design <amsanchez@knox.edu>
2021-01-16 15:02:45 -0600

    add simple footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+      <footer id="footer-wrap">
+      <h1>Thank you for visiting!</h1>
+
+    </footer>
+    );
+  }
+export default Footer;
